ab11efe113be2cc5771f518c4857f260a2861616
Kristijan Jakimov <kiko.jakimov@gmail.com>
2024-09-04 14:44:40 +0200

    second car added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("Toyota", "Raw4", "red", 2015, 0);
         System.out.println(car.speed);
-        car.accelerate(100);
-        System.out.println(car.speed);
-        car.accelerate();
-        System.out.println(car.speed);
+
+        Car carTwo = new Car("Porsche", "GT4", "red", 2015, 120);
+        carTwo.accelerate(40);
+        System.out.println(carTwo.speed);
     }
 }
